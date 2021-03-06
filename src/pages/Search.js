f43416d771eebc8d58800b5f2ae4cc468934a328
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2021-03-06 16:40:30 +0900

    Layout Headerコンポーネントの追加, Headerのスタイリング

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const Search = () => {
   return (
-    <div>
+    <Layout>
       <p>this is the search page.</p>
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const Watch = () => {
   return (
-    <div>
+    <Layout>
       <p>this is the watch page.</p>
-    </div>
+    </Layout>
   )
 }
 
